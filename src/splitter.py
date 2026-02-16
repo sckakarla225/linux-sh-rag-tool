45b92e45ceae3c690fd0271d34b52c6f75d2c6eb
Samhith Kakarla <samhith.kakarla@duke.edu>
2026-02-16 18:23:34 -0500

    man pages all cleaned

--- src/splitter.py
+++ src/splitter.py
@@ -1,4 +1,9 @@
 import re
 
+class ManPageSplitter:
+    def __init__(self, man_page_path: str):
+        self.man_page_path = man_page_path
+
 if __name__ == "__main__":
     print("Splitting man page...")
+    splitter = ManPageSplitter("data/cat.txt")
